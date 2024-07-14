c35fc2312eed617ffb30c0f2fec50607537aa217
m10c2 <tinkertailorsoldersailor@gmail.com>
2024-07-14 18:46:14 +0500

    created struct for simplicial complex

---
+++ homology.cpp
@@ -0,0 +1,30 @@
+﻿// homology.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+
+struct Simplex {
+public:
+    std::vector<int> vertices;
+    int dimention;
+
+    Simplex(const std::vector<int>& v) : vertices(v), dimention(v.size() - 1) {}
+    ~Simplex() {
+        vertices.clear();
+    }
+};
+
+struct SimplicialComplex {
+public:
+    std::vector<Simplex> simplices;
+
+    void addSimplex(const Simplex& s) {
+        simplices.push_back(s);
+    }
+};
+
+int main()
+{
+    return 0;
+}

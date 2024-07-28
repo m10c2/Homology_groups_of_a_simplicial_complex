609b096687ad483e9ba0fb4eae9f907fb8e11c4b
m10c2 <tinkertailorsoldersailor@gmail.com>
2024-07-28 19:34:11 +0500

    add TODO's and etc.

--- homology.cpp
+++ homology.cpp
@@ -4,27 +4,57 @@
 #include <iostream>
 #include <vector>
 
-struct Simplex {
+using std::vector;
+
+/*TODO: 
+1) добавить исключение при несостыковке размерностей
+2) добавить деструктор
+*/
+class Simplex {
 public:
-    std::vector<int> vertices;
+    vector<vector<int>> vertices;
     int dimention;
-
-    Simplex(const std::vector<int>& v) : vertices(v), dimention(v.size() - 1) {}
-    ~Simplex() {
-        vertices.clear();
-    }
+    
+    
+    Simplex(vector<vector<int>>& vs) : vertices(vs), dimention(vs.size() - 1) {}
 };
 
-struct SimplicialComplex {
+
+/*TODO:
+1)реализовать пустой симплекс, потому что некоторых размерностей может не быть
+2)конструктор, делающий из списка вершин цепной комплекс
+3)деструктор
+*/
+
+class ChainComplex {
 public:
-    std::vector<Simplex> simplices;
+    vector<vector<Simplex>> chain_complex;
 
-    void addSimplex(const Simplex& s) {
-        simplices.push_back(s);
+    ChainComplex() {
+
     }
-};
+    ~ChainComplex() {
+        for (int i = 0; i < chain_complex.size(); i++) {
+            for (int j = 0; j < chain_complex[i].size(); j++) {
+                //delete chain_complex[i][j];
+            }
+        }
+    }
+   
+    void inline addSimplex(const Simplex& s) {
+        chain_complex[s.dimention].push_back(s);
+    }
 
+};
+    
 int main()
 {
-    return 0;
+    vector<vector<int>> vertices = { {1,0,0},{0,1,0},{0,0,1} };
+    Simplex simplex(vertices);
+
+    std::cout << simplex.dimention << std::endl;
+
+
+
+    return 228;
 }

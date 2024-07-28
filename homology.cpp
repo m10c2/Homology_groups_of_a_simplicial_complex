881ef070dcc30b95b8f86160328a662c3775b496
m10c2 <tinkertailorsoldersailor@gmail.com>
2024-07-29 01:33:38 +0500

    added exceptions in Simplex

--- homology.cpp
+++ homology.cpp
@@ -3,48 +3,64 @@
 
 #include <iostream>
 #include <vector>
+#include <stdexcept>
 
 using std::vector;
+ 
 
-/*TODO: 
-1) добавить исключение при несостыковке размерностей
-2) добавить деструктор
-*/
 class Simplex {
 public:
     vector<vector<int>> vertices;
-    int dimention;
+    int dimension;
     
-    
-    Simplex(vector<vector<int>>& vs) : vertices(vs), dimention(vs.size() - 1) {}
+    Simplex(vector<vector<int>>& vs) : vertices(vs), dimension(vertices.size() - 1){
+        if (vs.empty()) {
+            throw std::invalid_argument("empty simplex");
+        }
+        int dim = vs[0].size();
+        for (const auto& vertex : vs) {
+            if (vertex.size() != dim) {
+                throw std::invalid_argument("all vertices must have the same dimetion");
+            }
+        }
+        if (vs.size() != vs[0].size()) {
+            throw std::invalid_argument("a simplex in n dimentions must have n+1 vertices"); 
+        }
+    }
+
+    void print() {
+        return;
+    }
 };
 
 
 /*TODO:
 1)реализовать пустой симплекс, потому что некоторых размерностей может не быть
-2)конструктор, делающий из списка вершин цепной комплекс
-3)деструктор
+2)реализовать конструктор, принимающий на вход список симплексов
+3)print
 */
 
 class ChainComplex {
 public:
     vector<vector<Simplex>> chain_complex;
 
-    ChainComplex() {
-
-    }
-    ~ChainComplex() {
-        for (int i = 0; i < chain_complex.size(); i++) {
-            for (int j = 0; j < chain_complex[i].size(); j++) {
-                //delete chain_complex[i][j];
-            }
+    ChainComplex(vector<Simplex>& ss) {
+        std::cout << "ChainComplex constructor" << std::endl;
+        for (const auto& simplex : ss) {
+            addSimplex(simplex);
         }
     }
-   
+  
     void inline addSimplex(const Simplex& s) {
-        chain_complex[s.dimention].push_back(s);
+        //проблема в том, что этого индекса может и не быть :(
+        chain_complex[s.dimension].push_back(s);//крашится на этом месте
+        std::cout << "nice" << std::endl;
     }
 
+    void print() {
+        return;
+    }
+    
 };
     
 int main()
@@ -52,9 +68,10 @@ int main()
     vector<vector<int>> vertices = { {1,0,0},{0,1,0},{0,0,1} };
     Simplex simplex(vertices);
 
-    std::cout << simplex.dimention << std::endl;
+    vector<Simplex> simplex_list = {simplex};
+    
+    //ChainComplex complex(simplex_list);
 
-
-
-    return 228;
+   
+    return 0;
 }
